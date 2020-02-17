00244d2643928e0e3375af9d5d89ea6e082a4dfd
Moringa School <loisemburu01@gmail.com>
2020-02-17 11:18:55 +0300

    added content to base.tml and modified it

--- awards_app/urls.py
+++ awards_app/urls.py
@@ -1,7 +1,12 @@
 from . import views
 from django.conf.urls import url
 
+
 urlpatterns=[
     url(r'^$', views.index, name='homepage'),
-   
+    url(r'^profile/$', views.myProfile, name='profile'),
+    url(r'^search/$', views.search_title, name='search_title'),
+    url(r'^details/(\d+)$', views.details, name='details'),
+    url(r'^api/profile/$', views.Profile_list.as_view()),
+    url(r'^api/projects/$', views.Project_list.as_view()),
 ]
