65a5dfb38dcc4f531db3b98d6dc61fdca7c77847
Moringa School <loisemburu01@gmail.com>
2020-02-17 11:25:18 +0300

    created a form for the profile

---
+++ awards_app/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Profile,Project,Review
+SCORES = [
+    (1,1),(2,2),
+    (3,3),(4,4),
+    (5,5),(6,6),
+    (7,7),(8,8),
+    (9,9),(10,10),
+]
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['user']
