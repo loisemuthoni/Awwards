51c513700e8bd4527cfc9443ba59fde9cc7c343e
Moringa School <loisemburu01@gmail.com>
2020-02-17 11:11:19 +0300

    did the index.html

--- awards_app/views.py
+++ awards_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Project,Review
 
 # Create your views here.
+# Create your views here.
+def index(request,**kwargs):
+    projects=Project.objects.all()[::-1]
+    proj_upload=ProjectUploadForm(request.POST, request.FILES)
+    if proj_upload.is_valid():
+        projo=proj_upload.save(commit=False)
+        projo.user=request.user
+        projo.save()
+        return HttpResponseRedirect(request.path_info)
+    else:
+        proj_upload=ProjectUploadForm()
+    context={
+        'projects':projects,
+        'proj_upload':proj_upload,
+    }
+    return render(request, 'index.html', locals())
