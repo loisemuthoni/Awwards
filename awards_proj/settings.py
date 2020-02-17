349e8d1c19aee07a8dd0a871076a8f88456dd386
Moringa School <loisemburu01@gmail.com>
2020-02-17 12:47:34 +0300

    created a db

--- awards_proj/settings.py
+++ awards_proj/settings.py
@@ -79,11 +79,12 @@ WSGI_APPLICATION = 'awards_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awwards',
+        'USER': 'moringa',
+    'PASSWORD':'lilo1234',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
