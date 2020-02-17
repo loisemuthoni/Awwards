6143d31d7f16d274145ab3faa867ef28cb99b880
Moringa School <loisemburu01@gmail.com>
2020-02-17 11:42:33 +0300

    created a detail function

--- awards_app/views.py
+++ awards_app/views.py
@@ -36,3 +36,24 @@ def myProfile(request,**kwargs):
         'user_posts':user_posts,
     }
     return render(request, 'profile.html', locals())
+
+def details(request, id):
+    current_site=Project.single_project(id)
+    current_user=request.user
+    proj_reviews=Review.objects.filter(project=current_site)
+    review_form=ReviewForm(request.POST)
+    if review_form.is_valid():
+        review=review_form.save(commit=False)
+        review.user=current_user
+        review.project=current_site
+        review.save()
+        return HttpResponseRedirect(request.path_info)
+    else:
+        review_form=ReviewForm()
+    context={
+        'current_user':current_user,
+        'current_site':current_site,
+        'review_form':review_form,
+        'proj_reviews':proj_reviews,
+    }
+    return render(request, 'proj_details.html',locals())
