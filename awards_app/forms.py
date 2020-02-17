62afe746bf521fcb5d1cf53f91178f9608d0079d
Moringa School <loisemburu01@gmail.com>
2020-02-17 11:51:12 +0300

    made a form for projectupload

--- awards_app/forms.py
+++ awards_app/forms.py
@@ -12,3 +12,8 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model=Profile
         exclude=['user']
+
+class ProjectUploadForm(forms.ModelForm):
+    class Meta:
+        model=Project
+        exclude=['user']        
