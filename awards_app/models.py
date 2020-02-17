8fe1b0917305ba5a111556ae9125fc7ccb9a842d
Moringa School <loisemburu01@gmail.com>
2020-02-17 11:20:51 +0300

    created a profile class

--- awards_app/models.py
+++ awards_app/models.py
@@ -2,3 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    avi=ImageField(manual_crop='')
+    bio=models.CharField(max_length=240)
+    phone=models.IntegerField(blank=True)
+    email=models.EmailField()
