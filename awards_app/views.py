cb67b848f736983770dd4a036c4c41ec617c02e4
Moringa School <loisemburu01@gmail.com>
2020-02-19 12:36:07 +0300

    changes

--- awards_app/views.py
+++ awards_app/views.py
@@ -75,7 +75,31 @@ def search_title(request):
     else:
         message="Looking for something, type it and hit search"
         return render(request, 'results.html', {'message':message})       
+"""awards_proj URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url,include 
+from django.contrib import admin
+from django.contrib.auth import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('awards_app.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page":'/'}),
+]
 class Profile_list(APIView):
     def get(self, request, format=None):
         all_profile=Profile.objects.all()
